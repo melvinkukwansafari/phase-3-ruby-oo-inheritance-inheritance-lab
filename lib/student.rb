926dd4aadea9f154e21ba98d6198f936cf6abbfb
melvinkukwansafari <melvin.kukwan@student.moringaschoolcom>
2023-02-16 09:59:36 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative 'user'
 
+class Student < User
+
+    @knowledge = []
+
+  def initialize( knowledge=[])
+   @knowledge = knowledge
+  end
+
+  def learn(str)
+    self.knowledge << str
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
+
+
+
